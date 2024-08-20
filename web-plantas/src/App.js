a6d469471de8413231309431089a366f964a5ee8
Joaquin <joaquinserra94@gmail.com>
2024-08-20 06:35:08 +0200

    WebPlantas

---
+++ web-plantas/src/App.js
@@ -0,0 +1,22 @@
+import Home from './Screens/Home'
+import Services from './Screens/Services';
+import HowWeWork from './Screens/HowWeWork';
+import Benefits from './Screens/Benefits';
+import Contact from './Screens/Contact';
+import Navbar from './Components/Navbar'; 
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <Services />
+      <HowWeWork />
+      <Benefits />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
